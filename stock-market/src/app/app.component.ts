1814d8782b815fd9cda2b8bc45ec4cfaa7f145a5
ding279054 <51145322+ding279054@users.noreply.github.com>
2019-06-05 21:54:24 +0800

    Add files via upload

--- stock-market/src/app/app.component.ts
+++ stock-market/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Stock } from 'src/app/model/stock';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stock-market';
+  public stockobj: Stock;
+  public stock: Stock;
+  private counter = 1;
+
+  ngOnInit(): void {
+    this.stockobj = new Stock('股票公司input', 'TSC', 85, 80);
+  }
+  // onToggle(stockobj: Stock) {
+    onToggle(test) {
+    // console.log('favorite for stock', stockobj, 'was triggered');
+    console.log('favorite for stock', test, 'was triggered');
+    this.stockobj.favorite = !this.stockobj.favorite;
+  }
+  changeStockObject() {
+    this.stockobj = new Stock('股票公司input' + this.counter++, 'TSC', 85, 80);
+  }
+  changeStockPrice() {
+    // ChangeDetectionStrategy.Default才會更新html
+    // 若為ChangeDetectionStrategy.OnPush則值會改變,但html不會刷新
+    this.stockobj.price += 10;
+  }
 }
--- stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Stock } from '../../model/stock';
 
 @Component({
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
-  styleUrls: ['./stock-item.component.css']
+  /* template: `
+  <div class="stock-con" *ngFor="let stock of stocks; even as isEven; index as i; trackBy: trackStockByCode" [class.even-item]="isEven">
+  <div class="name"><h3>{{stock.name}}</h3>-<h4>({{stock.code}})</h4></div>
+  <div class="price" [class.positive]="stock.isPositivechange()" [class.negative]="!stock.isPositivechange()"
+     >$ {{stock.price}}</div>
+  <button (click)="toggleFavorite($event, i)"
+  [disabled]="stock.favorite">Add to Favorite</button>
+</div>`, */
+  styleUrls: ['./stock-item.component.css'],
+  /* styles: [`
+  .stock-con {
+    border: 1px solid black;
+    border-radius: 5px;
+    display: inline-block;
+    padding: 10px;
+  }
+  .even-item {
+    border: 1px solid yellow;
+    border-radius: 10px;
+    display: inline-block;
+    padding: 10px;
+  }
+  .stock-con .name h3,.stock-con .name h4 {
+    display: inline-block;
+  }
+  .positive{
+    color:red;
+  }
+  .negative{
+    color:green;
+  }`] */
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StockItemComponent implements OnInit {
-  public stock: Stock;
+  // public stock: Stock;
+  @Input() public stock: Stock;
+  @Output() private toggleFavorite: EventEmitter<Stock>;
+  @Output() private outputtest: EventEmitter<string>;
+  public stocks: Array<Stock>;
   public stockClasses;
   public stockStyles;
+  public test = 'test';
   /* public name: string;
   public code: string;
   public price: number;
@@ -18,13 +54,21 @@ export class StockItemComponent implements OnInit {
   public favorite: boolean; */
 
 
-  constructor() { }
+  constructor() {
+    this.toggleFavorite = new EventEmitter<Stock>();
+    this.outputtest = new EventEmitter();
+   }
 
   ngOnInit() {
-    this.stock = new Stock('股票測試公司', 'TSC', 85, 80);
+    // this.stock = new Stock('股票測試公司', 'TSC', 85, 80);
+    this.stocks = [
+      new Stock('股票測試公司1', 'TSC', 85, 80),
+      new Stock('股票測試公司2', 'SSC', 15, 20),
+      new Stock('股票測試公司3', 'LSC', 888, 777)
+    ];
     const diff = (this.stock.price / this.stock.lastprice) - 1;
     const largeChange = Math.abs(diff) > 0.01;
-   /*  this.stockClasses = {
+    /* this.stockClasses = {
       "positive": this.stock.isPositivechange(),
       "negative": !this.stock.isPositivechange(),
       "large-change": largeChange,
@@ -44,8 +88,30 @@ export class StockItemComponent implements OnInit {
     this.favorite = false; */
   }
 
-  toggleFavorite(event) {
+  /* toggleFavorite(event) {
     console.log('toggling the favorite!', event);
     this.stock.favorite = !this.stock.favorite;
+  } */
+  // 加入最愛後disabled掉
+ /*  toggleFavorite(event, index) {
+    console.log('toggling the favorite!', index, event);
+    this.stocks[index].favorite = !this.stocks[index].favorite;
+  } */
+  /* toggleFavorite(event) {
+      this.stock.favorite = !this.stock.favorite;
+  } */
+  // 測試EventEmitter發射物件、字串
+  onToggleFavorite(event) {
+    // this.toggleFavorite.emit(this.stock);
+    this.outputtest.emit(this.test);
   }
+
+  // 以stock.code判斷DOM是否重複
+  trackStockByCode(index, stock) {
+    return stock.code;
+  }
+  changeStockPrice() {
+    this.stock.price += 5;
+  }
+
 }
