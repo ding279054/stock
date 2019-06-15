e1fbb7f85587e19930a54f91410401402eef752d
ding279054 <51145322+ding279054@users.noreply.github.com>
2019-06-15 22:03:55 +0800

    Add files via upload

--- stock-market/src/app/stock/create-stock/create-stock.component.ts
+++ stock-market/src/app/stock/create-stock/create-stock.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Stock } from '../../model/stock';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { FormBuilder } from '@angular/forms';
+let counter = 1;
 @Component({
   selector: 'app-create-stock',
   templateUrl: './create-stock.component.html',
@@ -9,17 +10,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateStockComponent {
   public nameControl = new FormControl();
-  public stockForm: FormGroup = new FormGroup({
+  public stockForm: FormGroup;
+  /* public stockForm: FormGroup = new FormGroup({
     name: new FormControl(null, Validators.required),
     code: new FormControl(null, [Validators.required, Validators.maxLength(2)]),
     price: new FormControl(0, [Validators.required, Validators.min(0)])
-  });
+  }); */
   public stock: Stock;
   public confirmed = false;
   public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
-  constructor() {
-    this.stock = new Stock('', '', 0, 0, 'NASDAQ');
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+    this.stock = new Stock('test ' + counter++, 'TST', 20, 10, 'NASDAQ');
    }
+   get code() { return this.stockForm.get('code'); }
+  /* constructor() {
+    this.stock = new Stock('', '', 0, 0, 'NASDAQ');
+   } */
   /* setStockPrice(price) {
     this.stock.price = price;
     this.stock.lastprice = price;
@@ -28,6 +35,15 @@ export class CreateStockComponent {
  /*  createStock() {
     console.log('createing stock', this.stock);
   } */
+
+  createForm() {
+    this.stockForm = this.fb.group({
+      name: ['test', Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
+
   createStock(stockForm) {
     // console.log('stock Form', stockForm);
     console.log('stock Form', stockForm.value);
@@ -41,6 +57,30 @@ export class CreateStockComponent {
 
   onSubmit() {
     // console.log('Name Control Value', this.nameControl.value);
-    console.log('stock Form Value', this.stockForm.value);
+    // console.log('stock Form Value', this.stockForm.value);
+    this.stock = Object.assign({}, this.stockForm.value);
+    console.log('儲存股票', this.stock);
+
   }
+
+  loadStockFromServer() {
+    this.stock = new Stock('test ' + counter++, 'TST', 20, 10, 'NASDAQ');
+    let stockFormModel = Object.assign({}, this.stock);
+    delete stockFormModel.lastprice;
+    delete stockFormModel.exchange;
+    delete stockFormModel.favorite;
+    // 使用setValue必須先移除無法對應的鍵值,留下的必須FormGroup內描述的完全一致
+    this.stockForm.setValue(stockFormModel);
+  }
+
+  patchStockForm() {
+    this.stock = new Stock(`test ${counter++}`, 'TST', 20, 10, 'NASDAQ');
+    // 使用patchValue不須移除無法對應的鍵值,只會處理對應得到的部分
+    this.stockForm.patchValue(this.stock);
+  }
+
+    resetForm() {
+      this.stockForm.reset();
+    }
+
 }
